4cd868f7d205e74459b13f8e93ae2194a12550d0
saisrk <i.am.songoku@gmail.com>
2017-07-09 12:10:43 +0530

    1. Modified the settings file for templates and static files 2. Added bootstrap files

--- bhajanamrutam/bhajanamrutam/settings.py
+++ bhajanamrutam/bhajanamrutam/settings.py
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static-storage"),
+]
